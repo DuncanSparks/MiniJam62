d39e4cf5e5c0f556a4aeb7e495c29e0c33b17087
Duncan Sparks <duncan.sprks@gmail.com>
2020-09-05 23:40:36 -0700

    Start on title screen (thanks Rami)

---
+++ Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip introSound;
+
+    void Start()
+    {
+        Invoke(nameof(Fadein), 1f);
+    }
+
+    void Fadein()
+    {
+        Controller.Singleton.PlaySoundOneShot(introSound, volume: 0.6f);
+        GetComponent<Animator>().Play("Fadein");
+    }
+}
