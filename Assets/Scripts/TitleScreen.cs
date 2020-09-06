6a6b358f3c9c6509d0cc341731c1d811b42031a0
Duncan Sparks <duncan.sprks@gmail.com>
2020-09-06 09:28:13 -0700

    Finish title screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -18,17 +18,48 @@ public class TitleScreen : MonoBehaviour
     AudioClip clickSound;
 
     [SerializeField]
+    GameObject creditsBackground;
+
+    [SerializeField]
+    GameObject creditsText;
+
+    [SerializeField]
     GameObject[] buttons;
+
+    bool creditsOpen = false;
 
     void Start()
     {
         Invoke(nameof(Fadein), 1f);
     }
 
+    void Update()
+    {
+        if (creditsOpen && Input.anyKeyDown)
+        {
+            Controller.Singleton.PlaySoundOneShot(clickSound, Random.Range(0.95f, 1.05f));
+            OpenCredits(false);
+        }
+    }
+
     void Fadein()
     {
         Controller.Singleton.PlaySoundOneShot(introSound, volume: 0.6f);
         GetComponent<Animator>().Play("Fadein");
+        Invoke(nameof(Fadein2), 1f);
+    }
+
+    void Fadein2()
+    {
+        ActivateButtons(true);
+    }
+
+    void ActivateButtons(bool activate)
+    {
+        foreach (GameObject but in buttons)
+        {
+            but.GetComponent<Button>().interactable = activate;
+        }
     }
 
     public void Hover()
@@ -39,16 +70,42 @@ public class TitleScreen : MonoBehaviour
     public void ClickStart()
     {
         Controller.Singleton.PlaySoundOneShot(clickSound, Random.Range(0.95f, 1.05f));
-        foreach (GameObject but in buttons)
-        {
-            but.GetComponent<Button>().enabled = false;
-        }
-
+        ActivateButtons(false);
         Invoke(nameof(ClickStart2), 0.5f);
     }
 
     void ClickStart2()
     {
         Controller.Singleton.ChangeScene(startScene.name, "Start");
+    }
+
+    public void ClickCredits()
+    {
+        Controller.Singleton.PlaySoundOneShot(clickSound, Random.Range(0.95f, 1.05f));
+        OpenCredits(true);
+    }
+
+    public void ClickExit()
+    {
+        Controller.Singleton.PlaySoundOneShot(clickSound, Random.Range(0.95f, 1.05f));
+        Controller.Singleton.PlaySoundOneShot(introSound, volume: 0.6f);
+        GetComponent<Animator>().Play("Fadeout");
+        Invoke(nameof(ClickExit2), 0.8f);
+    }
+
+    void ClickExit2()
+    {
+        #if UNITY_EDITOR
+             UnityEditor.EditorApplication.isPlaying = false;
+         #else
+             Application.Quit();
+         #endif
+    }
+
+    void OpenCredits(bool open)
+    {
+        creditsBackground.SetActive(open);
+        creditsText.SetActive(open);
+        creditsOpen = open;
     }
 }
