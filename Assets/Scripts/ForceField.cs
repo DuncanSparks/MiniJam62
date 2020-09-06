ce6dfd1619c2f5185cd76348313c4e00c9b8e33f
Duncan Sparks <duncan.sprks@gmail.com>
2020-09-06 13:59:27 -0700

    Add force field

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+	ParticleSystem particles;
+	new BoxCollider collider;
+	AudioSource sound;
+
+	void Start()
+	{
+		particles = GetComponentInChildren<ParticleSystem>();
+		collider = GetComponent<BoxCollider>();
+		sound = GetComponent<AudioSource>();
+	}
+
+    public void DisableForceField()
+	{
+		particles.Stop();
+		collider.enabled = false;
+		sound.Play();
+	}
+}
