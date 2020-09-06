8cf4a5a96a18113b1bf3167f8ffa792d9ffc3531
BMarcelus <bmdizon@uci.edu>
2020-09-06 11:35:41 -0700

    paintable forrest

---
+++ Assets/Paintable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    [SerializeField]
+    bool playerColors = true;
+    [SerializeField]
+    bool enemyColors = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other) {
+        if ((playerColors && other.gameObject.tag == "PlayerProjectile") ||
+            (enemyColors && other.gameObject.tag == "EnemyHitbox"))
+        {
+            GetComponent<Renderer>().material = other.gameObject.GetComponentInChildren<Renderer>().material;  
+            Controller.Singleton.ShowComicText("Splat", other.gameObject.transform.position, Camera.main);
+        }
+    }
+}
